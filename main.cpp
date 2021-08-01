3241161fdedfa82449534c02734f1c0a25e1d720
TylerHornak <68288921+TylerHornak@users.noreply.github.com>
2021-08-01 16:18:00 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <map>
 #include "Entry.h"
 #include "BTreeNode.h"
 
@@ -14,6 +15,12 @@ using namespace std;
 // Declare any methods in the main file here, before implementing them below
 void loadData(string filePath, vector<Entry*>& entries);
 //void graphData(vector<vector<Entry*>>& entries);
+float Selection(Entry* entry, int& inputChoice);
+void merge(vector<Entry*>& arr, int left, int mid, int right, int choice);
+void mergeSort(vector<Entry*>& arr, int left, int right, int choice);
+void quickSort(vector<Entry*>& arr, int low, int high, int choice);
+int split(vector<Entry*>& arr, int low, int high, int choice);
+void generateMap(map<float, vector<Entry*>>& map, vector<Entry*>& arr, int choice);
 
 
 // Declare any global variables here
@@ -66,18 +73,49 @@ int main() {
     }
     vector<vector<Entry*>> data = { v1, v2, v3, v4, v5 };
     //graphData(data);
-    BTreeNode* head = nullptr;
+    /*BTreeNode* head = nullptr;
     int choice = 1;
     head = head->InsertEntry(v1[0], head, choice);
     head = head->InsertEntry(v1[1], head, choice);
     head = head->InsertEntry(v1[2], head, choice);
 
-   
+
     head->keyList[0]->PrintEntry();
 
     //head->keyList[0]->PrintEntry();
     //head->keyList[1]->PrintEntry();
-    //head->keyList[2]->PrintEntry();
+    //head->keyList[2]->PrintEntry();*/
+
+    vector<Entry*> vTest;
+
+    /*for (int i = 0; i < 5; i++) {
+        v1[i]->PrintEntry();
+    }
+
+    for (int i = 0; i < 5; i++) {
+        vTest.push_back(v1[i]);
+    }*/
+
+    //quickSort(v1, 0, v1.size() - 1, 4);
+    
+    //cout << endl << endl << endl;
+
+    /*for (int i = 0; i < 100; i++) {
+        v1[i]->PrintEntry();
+    }*/
+
+    map<float, vector<Entry*>> testMap;
+
+    generateMap(testMap, v1, 4);
+
+    int t = 0;
+    for (auto iter = testMap.begin(); iter != testMap.end(); ++iter) {
+        t++;
+        if (t == 5) { break; }
+        for (int i = 0; i < iter->second.size(); i++) {
+            iter->second[i]->PrintEntry();
+        }
+    }
 
 }
 
@@ -112,16 +150,12 @@ void loadData(string filePath, vector<Entry*>& entries) {
 
 
 /*void graphData(vector<vector<Entry*>>& data) {
-
     // Initialize SFML window size and window
     int windowWidth = 1200;
     int windowHeight = 600;
-
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Data Visualization", sf::Style::Titlebar | sf::Style::Close);
-
     // Initialize graph object
     graph = new Graph(data, window);
-
     // Show the graph each frame
     while (window.isOpen()) {
         sf::Event event;
@@ -134,6 +168,115 @@ void loadData(string filePath, vector<Entry*>& entries) {
         graph->show(window);
         window.display();
     }
-
     delete graph;
 }*/
+
+float Selection(Entry* entry, int& inputChoice) {
+    if (inputChoice == 0) { return (float)entry->dateInt; }
+    else if (inputChoice == 1) { return (float)entry->total; }
+    else if (inputChoice == 2) { return (float)entry->changeInTotal; }
+    else if (inputChoice == 3) { return entry->totalPerThou; }
+    else if (inputChoice == 4) { return entry->changeInTotalPerThou; }
+}
+
+void merge(vector<Entry*>& arr, int left, int mid, int right, int choice) {
+    int xCount = mid - left + 1;
+    int yCount = right - mid;
+
+    vector<Entry*> X, Y;
+    for (int i = 0; i < xCount; i++) {
+        X.push_back(arr[left + i]);
+    }
+    for (int i = 0; i < yCount; i++) {
+        Y.push_back(arr[mid + 1 + i]);
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < xCount && j < yCount) {
+        if (Selection(X[i], choice) <= Selection(Y[j], choice)) {
+            arr[k] = X[i];
+            i++;
+        }
+        else {
+            arr[k] = Y[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < xCount) {
+        arr[k] = X[i];
+        i++;
+        k++;
+    }
+
+    while (j < yCount) {
+        arr[k] = Y[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<Entry*>& arr, int left, int right, int choice) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid, choice);
+        mergeSort(arr, mid + 1, right, choice);
+        merge(arr, left, mid, right, choice);
+    }
+}
+
+void quickSort(vector<Entry*>& arr, int low, int high, int choice) {
+    if (low < high) {
+        int piv = split(arr, low, high, choice);
+
+        /*for (int i = 0; i < 5; i++) {
+            cout << arr[i]->changeInTotalPerThou << endl;;
+        }
+        cout << endl;*/
+
+        quickSort(arr, low, piv - 1, choice);
+        quickSort(arr, piv + 1, high, choice);
+    }
+}
+
+int split(vector<Entry*>& arr, int low, int high, int choice) {
+    int up = low;
+    int down = high;
+    
+    Entry* tempE;
+
+    while (up < down) {
+        for (int i = up; i < high; i++) {
+            if (Selection(arr[up], choice) > Selection(arr[low], choice)) {
+                break;
+            }
+            up++;
+        }
+        for (int i = high; i > low; i--) {
+            if (Selection(arr[down], choice) < Selection(arr[low], choice)) {
+                break;
+            }
+            down--;
+        }
+        if (up < down) {
+            tempE = arr[up];
+            arr[up] = arr[down];
+            arr[down] = tempE;
+        }
+    }
+    tempE = arr[low];
+    arr[low] = arr[down];
+    arr[down] = tempE;
+    return down;
+
+}
+
+void generateMap(map<float, vector<Entry*>>& map, vector<Entry*>& arr, int choice) {
+    for (int i = 0; i < arr.size(); i++) {
+        map[Selection(arr[i], choice)].push_back(arr[i]);
+    }
+}
